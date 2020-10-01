ec38437472bb1983ef4c87892bf9d25de8900dc9
duranka-kads <durankaranaweera@gmail.com>
2020-10-01 21:10:11 +0530

    create allocation model

---
+++ src/spm/AllocationsModel.java
@@ -0,0 +1,389 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spm;
+
+/**
+ *
+ * @author User
+ */
+public class AllocationsModel {
+     private int roomTid;
+     private String name;
+     private String tag;
+     private int aRid;
+     private int aTid;
+      private int roomSubID;
+      private String subName;
+      private int subID;
+      private String empName;
+      private int roomEidID;
+      private int eid;
+      private int roomSubGroupId;
+      private String subGroupNo;
+      private int aSid;
+      
+      //for session
+      private int sessionRID;
+      private String sessionDetails;
+      private String subCode;
+      private int asessionID;
+      private int consesessionID;
+      private String consessName;
+      
+       private String consessName1;
+        private String consessName2;
+         private String consessName3;
+         
+         
+         //timeslot
+         private String atime;
+      
+      
+      
+      
+      
+      
+      
+     
+     
+
+    /**
+     * @return the roomTid
+     */
+    public int getRoomTid() {
+        return roomTid;
+    }
+
+    /**
+     * @param roomTid the roomTid to set
+     */
+    public void setRoomTid(int roomTid) {
+        this.roomTid = roomTid;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the tag
+     */
+    public String getTag() {
+        return tag;
+    }
+
+    /**
+     * @param tag the tag to set
+     */
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    /**
+     * @return the aRid
+     */
+    public int getaRid() {
+        return aRid;
+    }
+
+    /**
+     * @param aRid the aRid to set
+     */
+    public void setaRid(int aRid) {
+        this.aRid = aRid;
+    }
+
+    /**
+     * @return the aTid
+     */
+    public int getaTid() {
+        return aTid;
+    }
+
+    /**
+     * @param aTid the aTid to set
+     */
+    public void setaTid(int aTid) {
+        this.aTid = aTid;
+    }
+
+    /**
+     * @return the roomSubID
+     */
+    public int getRoomSubID() {
+        return roomSubID;
+    }
+
+    /**
+     * @param roomSubID the roomSubID to set
+     */
+    public void setRoomSubID(int roomSubID) {
+        this.roomSubID = roomSubID;
+    }
+
+    /**
+     * @return the subName
+     */
+    public String getSubName() {
+        return subName;
+    }
+
+    /**
+     * @param subName the subName to set
+     */
+    public void setSubName(String subName) {
+        this.subName = subName;
+    }
+
+    /**
+     * @return the subID
+     */
+    public int getSubID() {
+        return subID;
+    }
+
+    /**
+     * @param subID the subID to set
+     */
+    public void setSubID(int subID) {
+        this.subID = subID;
+    }
+
+    /**
+     * @return the empName
+     */
+    public String getEmpName() {
+        return empName;
+    }
+
+    /**
+     * @param empName the empName to set
+     */
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    /**
+     * @return the roomEidID
+     */
+    public int getRoomEidID() {
+        return roomEidID;
+    }
+
+    /**
+     * @param roomEidID the roomEidID to set
+     */
+    public void setRoomEidID(int roomEidID) {
+        this.roomEidID = roomEidID;
+    }
+
+    /**
+     * @return the eid
+     */
+    public int getEid() {
+        return eid;
+    }
+
+    /**
+     * @param eid the eid to set
+     */
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    /**
+     * @return the roomSubGroupId
+     */
+    public int getRoomSubGroupId() {
+        return roomSubGroupId;
+    }
+
+    /**
+     * @param roomSubGroupId the roomSubGroupId to set
+     */
+    public void setRoomSubGroupId(int roomSubGroupId) {
+        this.roomSubGroupId = roomSubGroupId;
+    }
+
+    /**
+     * @return the subGroupNo
+     */
+    public String getSubGroupNo() {
+        return subGroupNo;
+    }
+
+    /**
+     * @param subGroupNo the subGroupNo to set
+     */
+    public void setSubGroupNo(String subGroupNo) {
+        this.subGroupNo = subGroupNo;
+    }
+
+    /**
+     * @return the aSid
+     */
+    public int getaSid() {
+        return aSid;
+    }
+
+    /**
+     * @param aSid the aSid to set
+     */
+    public void setaSid(int aSid) {
+        this.aSid = aSid;
+    }
+
+    /**
+     * @return the sessionID
+     */
+    public int getSessionRID() {
+        return sessionRID;
+    }
+
+    /**
+     * @param sessionID the sessionID to set
+     */
+    public void setSessionRID(int sessionRID) {
+        this.sessionRID = sessionRID;
+    }
+
+    /**
+     * @return the sessionDetails
+     */
+    public String getSessionDetails() {
+        return sessionDetails;
+    }
+
+    /**
+     * @param sessionDetails the sessionDetails to set
+     */
+    public void setSessionDetails(String sessionDetails) {
+        this.sessionDetails = sessionDetails;
+    }
+
+    /**
+     * @return the subCode
+     */
+    public String getSubCode() {
+        return subCode;
+    }
+
+    /**
+     * @param subCode the subCode to set
+     */
+    public void setSubCode(String subCode) {
+        this.subCode = subCode;
+    }
+
+    /**
+     * @return the asessionID
+     */
+    public int getAsessionID() {
+        return asessionID;
+    }
+
+    /**
+     * @param asessionID the asessionID to set
+     */
+    public void setAsessionID(int asessionID) {
+        this.asessionID = asessionID;
+    }
+
+    /**
+     * @return the consesessionID
+     */
+    public int getConsesessionID() {
+        return consesessionID;
+    }
+
+    /**
+     * @param consesessionID the consesessionID to set
+     */
+    public void setConsesessionID(int consesessionID) {
+        this.consesessionID = consesessionID;
+    }
+
+    /**
+     * @return the consessName
+     */
+    public String getConsessName() {
+        return consessName;
+    }
+
+    /**
+     * @param consessName the consessName to set
+     */
+    public void setConsessName(String consessName) {
+        this.consessName = consessName;
+    }
+
+    /**
+     * @return the consessName1
+     */
+    public String getConsessName1() {
+        return consessName1;
+    }
+
+    /**
+     * @param consessName1 the consessName1 to set
+     */
+    public void setConsessName1(String consessName1) {
+        this.consessName1 = consessName1;
+    }
+
+    /**
+     * @return the consessName2
+     */
+    public String getConsessName2() {
+        return consessName2;
+    }
+
+    /**
+     * @param consessName2 the consessName2 to set
+     */
+    public void setConsessName2(String consessName2) {
+        this.consessName2 = consessName2;
+    }
+
+    /**
+     * @return the consessName3
+     */
+    public String getConsessName3() {
+        return consessName3;
+    }
+
+    /**
+     * @param consessName3 the consessName3 to set
+     */
+    public void setConsessName3(String consessName3) {
+        this.consessName3 = consessName3;
+    }
+
+    /**
+     * @return the atime
+     */
+    public String getAtime() {
+        return atime;
+    }
+
+    /**
+     * @param atime the atime to set
+     */
+    public void setAtime(String atime) {
+        this.atime = atime;
+    }
+   
+}
